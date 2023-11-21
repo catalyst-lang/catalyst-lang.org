1ad9a0481f532133b37f3859e3ddb34e27dae006
Bas du Pré <me@basdupre.com>
2023-11-21 10:05:21 +0100

    updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'catalyst-lang', // Usually your GitHub org/user name.
   projectName: 'catalyst-lang.org', // Usually your repo name.
-  deploymentBranch: 'main',
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
